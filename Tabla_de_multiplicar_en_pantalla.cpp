fe2622ffceab1d22f40fde5f1bafeaed26028167
Awpariq2024 <awpariq@unjbg.edu.pe>
2024-06-13 00:36:48 -0500

    Se agrega el ejercicio 1 de estructuras repetitivas

---
+++ Tabla_de_multiplicar_en_pantalla.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int contador=1,multiplo=7;
+	int n;
+	
+	cout<<"Ingrese hasta que numero quiere la tabla del 7: ";
+	cin>>n;
+	
+	while (contador<=n){
+		cout<<"7 x ";
+		cout<<contador<<"= ";
+		cout<<multiplo<<endl;
+		multiplo=multiplo+7;
+		contador=contador+1;
+	}
+	
+	return 0;
+}
